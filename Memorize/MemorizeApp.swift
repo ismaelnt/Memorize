b032ce4f17f872856dfaca3acd83ac090b2d69e0
Ismael Neto <ismaelnt97@gmail.com>
2023-05-31 07:51:26 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ismael Neto on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
