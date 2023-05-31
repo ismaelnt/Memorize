791a7643dfc6940f4302570f2b7929166b98860a
Ismael Neto <ismaelnt97@gmail.com>
2023-05-31 09:55:04 -0300

    feat: add CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,21 +1,31 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Ismael Neto on 31/05/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
         }
+        .foregroundColor(Color.orange)
         .padding()
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
